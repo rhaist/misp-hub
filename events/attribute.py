4ecb14c9f9391c8936f7d5908fa53ee12fbe0d99
Robert Haist <rhaist@mailbox.org>
2017-02-14 00:19:26 +0100

    Updated the Attribute model

--- events/attribute.py
+++ events/attribute.py
@@ -2,38 +2,43 @@ import uuid
 from django.db import models
 from sharing_groups.models import SharingGroup
 
-class CategoryType(models.Model):
-    description = models.CharField(max_length=1024)
-    form_description = models.CharField(max_length=1024)
-
 
 class AttributeType(models.Model):
-    description = models.CharField(max_length=1024)
-    form_description = models.CharField(max_length=1024)
-    default_category = models.ForeignKey(CategoryType)
+    '''
+    An AttributeType consists of a prefix and an optional suffix separated by a
+    pipe symbol. For example: domain|ip or filename|sha512
+    '''
+    description = models.CharField(max_length=1024, blank=True)
+    value_prefix = models.TextField(blank=False)
+    value_suffix = models.TextField(blank=True)
     to_ids = models.BooleanField(default=False)
 
+    def __str__(self):
+        if self.value_suffix.blank:
+            return '%s' % (self.value_prefix)
+        else:
+            return '%s|%s' % (self.value_prefix, self.value_suffix)
 
-class AttributeGroup(models.Model):
+
+class AttributeCategory(models.Model):
     description = models.CharField(max_length=1024)
-    attributes = models.ManyToManyField(AttributeType)
+    attr_types = models.ManyToManyField(AttributeType)
 
 
 class Attribute(models.Model):
     DISTRIBUTION_CHOICES = (
-        (0, "Your organisation only"),
-        (1, "This community only"),
-        (2, "Connected communities"),
-        (3, "All communities"),
-        (4, "Sharing group"),
-        (5, "Inherit Event"),
-    )
+        (0, 'Your organisation only'),
+        (1, 'This community only'),
+        (2, 'Connected communities'),
+        (3, 'All communities'),
+        (4, 'Sharing group'),
+        (5, 'Inherit Event'), )
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     # Is 'type' in the original model. But this is a keyword in most languages
     # and should not be named this way.
     attr_type = models.ForeignKey(AttributeType, blank=False)
-    category = models.ForeignKey(CategoryType, blank=False)
+    category = models.ForeignKey(AttributeCategory, blank=False)
     to_ids = models.BooleanField(default=False)
     distribution = models.IntegerField(default=0, choices=DISTRIBUTION_CHOICES)
     # The original model only has 'timestamp' we clearify here
