79c9fff8f98fec66fa1cd32bd019437f5bbfea82
Robert Haist <rhaist@mailbox.org>
2017-02-14 11:56:09 +0100

    Removed galaxy and feeds app for now

--- galaxy/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GalaxyConfig(AppConfig):
-    name = 'galaxy'
--- galaxy/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Galaxy(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=1024)
-    galaxy_type =
-    description = models.TextField()
-    version = models.CharField(max_length=1024)
